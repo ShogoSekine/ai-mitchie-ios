e79a150a8230c9d5da38eeb2cf6d876c8be536ee
ShogoSekine <shogo.sekine@smart-sou.co.jp>
2026-02-23 18:15:21 +0900

    1weekに変更

--- ai-mitchie-ios/ai_mitchie_iosApp.swift
+++ ai-mitchie-ios/ai_mitchie_iosApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct ai_mitchie_iosApp: App {
@@ -13,5 +14,6 @@ struct ai_mitchie_iosApp: App {
         WindowGroup {
             MainMenuView()
         }
+        .modelContainer(for: DailySessionModel.self)
     }
 }
